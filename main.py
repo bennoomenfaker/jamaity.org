1716f49272f47244937b8df23cf9ba2b40d1c527
FAKER BEN NOOMEN <98341511+bennoomenfaker@users.noreply.github.com>
2025-03-12 16:41:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI
+import uvicorn
 from app.routes.jobs import jobs_router
 from app.routes.reports import reports_router
+from fastapi.middleware.cors import CORSMiddleware
+import os
 
 app = FastAPI()
 
+
+# Configurer CORS
+origins = [
+    "http://localhost:3000",
+     "*"
+    
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(jobs_router, prefix="/jobs")
 app.include_router(reports_router, prefix="/reports")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Utilise le port de Render ou 8000 par défaut
+    uvicorn.run(app, host="0.0.0.0", port=port)
